e4716e6d8cb740c779674735e455430040e86eb2
Corbin <forstupidityonly@gmail.com>
2021-10-18 14:04:45 -0500

    fix the fuckin list type import bs

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """sum_list takes input_list of float's and returns sum"""
 
+from typing import List
 
-def sum_list(input_list: list) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """list of floats"""
     rtnVal = 0
     for x in input_list:
