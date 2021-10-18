032fb39bb21f3d58310c904f55b4afe760625dd4
Corbin <forstupidityonly@gmail.com>
2021-10-18 14:00:04 -0500

    for a test

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,4 +2,9 @@
 """sum_list takes input_list of float's and returns sum"""
 
 
-
+def sum_list(input_list: list) -> float:
+    """list of floats"""
+    rtnVal = 0
+    for x in input_list:
+        rtnVal += x
+    return rtnVal
