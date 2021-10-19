498c7a07f1ea2541653e7e3e92ca3077fae0b84c
Corbin <forstupidityonly@gmail.com>
2021-10-19 15:25:56 -0500

    Everything is documented

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" """
+"""Everything is documented"""
 import asyncio
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """ """
+    """Everything is documented"""
     myList = asyncio.run(wait_n(n, max_delay))
     myFloat = 0.0
     for i in myList:
