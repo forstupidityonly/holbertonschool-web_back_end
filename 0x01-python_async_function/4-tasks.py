ab717f894e75a2c0d4cf78fbb815e5816a0d2fea
Corbin <forstupidityonly@gmail.com>
2021-10-19 15:43:36 -0500

    why the hell

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""call wait_random n times"""
+
+
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """the rtn needs to be in assending order? concurrency???"""
+    i = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    j = [await i for i in asyncio.as_completed(i)]
+    return j
