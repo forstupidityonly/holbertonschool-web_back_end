3b48045eda9f8f3c3f8f8012a6056a8982431d0f
Corbin <forstupidityonly@gmail.com>
2021-10-25 08:10:49 -0500

    no time stamps or ordered dic:

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""class FIFOCache inherits from BaseCaching and is a caching system"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """first in first out caching"""
+
+    def __init__(self):
+        """what is overloading?"""
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """track order added w/ self.order"""
+        if (key is not None and item is not None):
+            self.cache_data[key] = item
+            if (key in self.order):
+                self.order.remove(key)
+                self.order.append(key)
+            else:
+                self.order.append(key)
+            if (len(self.order) > BaseCaching.MAX_ITEMS):
+                yeet = self.order.pop(0)
+                del self.cache_data[yeet]
+                print("DISCARD: " + yeet)
+
+    def get(self, key):
+        """rtn item by key"""
+        if (key is not None and key in self.order):
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data[key]
+        return None
