4895f552867f593581393ae517e5bafdc64327b7
Corbin <forstupidityonly@gmail.com>
2021-10-27 15:59:25 -0500

    first?

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""helper function rtn tuple"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those 
+    particular pagination parameters."""
+    return ((page - 1) * page_size, page * page_size)
