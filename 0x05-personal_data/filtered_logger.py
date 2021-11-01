e45e83a9c94fa69ecd80851bb181c9b98ee32ed9
Corbin <forstupidityonly@gmail.com>
2021-11-01 13:05:23 -0500

    im glad these tell me whats wrong. ps i hate regex

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -5,16 +5,15 @@ import re
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                separator: str) -> str:
+                 separator: str) -> str:
     """
         fields: a list of strings representing all fields to obfuscate
         redaction: a string representing by what the field will be obfuscated
         message: a string representing the log line
-        separator: a string representing by which character is separating all fields in the log line (message)
-
+        separator: a string representing by which character is separating all
         must use regex and be less than 5 lines long
     """
     for field in fields:
         message = re.sub(field + "=" + ".+?" + separator,
-                        field + "=" + redaction + separator, message)
+                         field + "=" + redaction + separator, message)
     return message
