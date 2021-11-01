5650f01357d0ef7db54f06009f32efc7d41f2348
Corbin <forstupidityonly@gmail.com>
2021-11-01 13:02:47 -0500

    did i do it?

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""pii and non-pii"""
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                separator: str) -> str:
+    """
+        fields: a list of strings representing all fields to obfuscate
+        redaction: a string representing by what the field will be obfuscated
+        message: a string representing the log line
+        separator: a string representing by which character is separating all fields in the log line (message)
+
+        must use regex and be less than 5 lines long
+    """
+    for field in fields:
+        message = re.sub(field + "=" + ".+?" + separator,
+                        field + "=" + redaction + separator, message)
+    return message
