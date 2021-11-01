9298906722b08cb76359fa25ef045af063ccf30a
Corbin <forstupidityonly@gmail.com>
2021-11-01 13:41:29 -0500

    task 1 documented

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -14,10 +14,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """init function"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """call filter_datum with init vals"""
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record),
                             self.SEPARATOR)
