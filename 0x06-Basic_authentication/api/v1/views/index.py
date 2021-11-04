f61ab566a26ac90e764902ea592bc057df9dfabb
Corbin <forstupidityonly@gmail.com>
2021-11-04 10:55:42 -0500

    pycodestyle blank lines added

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -3,6 +3,8 @@
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
+
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
@@ -22,6 +24,7 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
