d7fc57d34259033ed88b45ed4e9c6f185668cf04
Corbin <forstupidityonly@gmail.com>
2021-11-04 15:57:32 -0500

    idk if ill do the adv

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,8 +5,9 @@ basic authentication for auth
 import binascii
 import json
 import base64
-
+from typing import TypeVar
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -35,3 +36,25 @@ class BasicAuth(Auth):
         if (not isinstance(deco, str) or ':' not in deco):
             return (None, None)
         return (deco[:deco.find(':')], deco[deco.find(':') + 1:])
+
+    def user_object_from_credentials(
+            self,
+            user_email: str, user_pwd: str) -> TypeVar('User'):
+        """email and password to make user"""
+        if isinstance(user_email, str) and isinstance(user_pwd, str):
+            User.load_from_file()
+            users_with_email = User.search(
+                {'email': user_email})
+            if users_with_email:
+                for user in users_with_email:
+                    if user.is_valid_password(pwd=user_pwd):
+                        return user
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """overloads Auth and retrieves the User instance for a request:"""
+        header = self.authorization_header(request)
+        b64 = self.extract_base64_authorization_header(header)
+        decode = self.decode_base64_authorization_header(b64)
+        user, pwd = self.extract_user_credentials(decode)
+        return self.user_object_from_credentials(user, pwd)
