2aabdcfc5b4bfa6232b9314cc33c618727c209b5
Corbin <forstupidityonly@gmail.com>
2021-11-04 14:53:33 -0500

    the right output

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,3 +10,10 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """basic authentication"""
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """fetch Base64 in header"""
+        if (isinstance(authorization_header, str) and
+                authorization_header.startswith('Basic ')):
+            return authorization_header[6:]
