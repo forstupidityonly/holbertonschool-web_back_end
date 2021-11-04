519333a1ba7ca809cdf803098c5c9a536c46ecf8
Corbin <forstupidityonly@gmail.com>
2021-11-04 12:19:34 -0500

    this is

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 """api auth"""
 from flask import request
+from typing import List, TypeVar
+
 
 class Auth:
     """api auth"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """used later"""
-
+        return False
 
     def authorization_header(self, request=None) -> str:
-        """no idea"""
-
+        """used later"""
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Return None"""
         return None
+
