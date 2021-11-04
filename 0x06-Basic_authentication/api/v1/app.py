a1fac8c5907c98c826feefd4789ef8679c15d771
Corbin <forstupidityonly@gmail.com>
2021-11-04 14:27:24 -0500

    thats what we was missing

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -39,6 +39,18 @@ def forbidden(error) -> str:
     return jsonify({"error": "Forbidden"}), 403
 
 
+@app.before_request
+def before_request():
+    """ Before request"""
+    if auth and auth.require_auth(request.path, ['/api/v1/status/',
+                                                 '/api/v1/unauthorized/',
+                                                 '/api/v1/forbidden/']):
+        if not auth.authorization_header(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
