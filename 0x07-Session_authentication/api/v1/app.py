d61702af3dcb129d8ee65a8504d322817a0c8c8a
Corbin <forstupidityonly@gmail.com>
2021-11-09 13:15:39 -0600

    irs alive

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -57,4 +57,4 @@ def before_request():
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port
+    app.run(host=host, port=port)
