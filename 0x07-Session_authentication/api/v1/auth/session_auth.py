0377bd94b61b736fc89b7a6222c8b546216e71ec
Corbin <forstupidityonly@gmail.com>
2021-11-10 03:52:12 -0600

    hope

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -6,3 +6,12 @@ from api.v1.auth.auth import Auth
 
 class SessionAuth(Auth):
     """session auth"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """make session id for user"""
+        if user_id is None or type(user_id) is not str:
+            return None
+        Session_ID = str(uuid4())
+        self.user_id_by_session_id[Session_ID] = user_id
+        return Session_ID
