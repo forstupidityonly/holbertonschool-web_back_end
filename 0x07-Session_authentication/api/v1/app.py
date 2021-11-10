477d12d65dd6010da7fe8e04fe5d07fa8804b8f8
Corbin <forstupidityonly@gmail.com>
2021-11-10 04:50:15 -0600

    its not authing correctle...

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -47,11 +47,14 @@ def before_request():
     """ Before request"""
     if auth and auth.require_auth(request.path, ['/api/v1/status/',
                                                  '/api/v1/unauthorized/',
-                                                 '/api/v1/forbidden/']):
+                                                 '/api/v1/forbidden/',
+                                                 '/api/v1/auth_session/login/']):
         if not auth.authorization_header(request):
             abort(401)
         if not auth.current_user(request):
             abort(403)
+        if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+            abort(401)
         request.current_user = auth.current_user(request)
 
 
