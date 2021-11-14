053f9075c400e7ccf78caa4ec75945be57f540cc
Corbin <forstupidityonly@gmail.com>
2021-11-14 13:35:29 -0600

    the hell is wrog

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -33,6 +33,6 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """add user"""
         user = User(email=email, hashed_password=hashed_password)
-        self.__session.add(user)
-        self.__session.commit()
+        self._session.add(user)
+        self._session.commit()
         return user
