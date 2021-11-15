8528c2433206f65199dfb066d7bd777207701a7f
Corbin <forstupidityonly@gmail.com>
2021-11-15 13:22:08 -0600

    not sure what is unanitated

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -31,7 +31,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
-        """add user"""
+        """The method should save the user to the database"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
