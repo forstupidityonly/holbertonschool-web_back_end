050653d6c22627f530523b531f0637d6c421b7fe
Corbin <forstupidityonly@gmail.com>
2021-11-16 14:28:50 -0600

    just return the bycript too include errormessages?

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-"""authentication of password for now"""
+"""authentication functions"""
 import bcrypt
 
 
 def _hash_password(pswrd: str) -> str:
     """use bcrypt.hashpw on pswrd"""
     salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(pswrd.encode('utf-8'), salt)
-    return hashed
+    return bcrypt.hashpw(pswrd.encode('utf-8'), salt)
