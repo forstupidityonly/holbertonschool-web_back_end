b031032a60b766eee9d086854ff945efb47b4ad4
Corbin <forstupidityonly@gmail.com>
2021-11-16 16:20:04 -0600

    methodS

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """flask app"""
-
 from auth import Auth
 from flask import Flask, jsonify
 
@@ -8,10 +7,11 @@ AUTH = Auth()
 app = Flask(__name__)
 
 
-@app.route('/', method=['GET'], strict_slashes=False)
+@app.route('/', methods=['GET'], strict_slashes=False)
 def taskSix() -> str:
     """task six"""
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
