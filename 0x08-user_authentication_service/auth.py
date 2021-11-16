97b401306c91471091c2a24339ed5dd1b926b7fa
Corbin <forstupidityonly@gmail.com>
2021-11-16 14:15:57 -0600

    https://howtodoinjava.com/python/modules/python-bcrypt-hash-password/

---
+++ 0x08-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""authentication of password for now"""
+import bcrypt
+
+
+def _hash_password(pswrd: str) -> bytes:
+    """use bcrypt.hashpw on pswrd"""
+    passwd = b'pswrd'
+    hashed = bcrypt.hashpw(passwd, bcrypt.gensalt())
+    return hashed
