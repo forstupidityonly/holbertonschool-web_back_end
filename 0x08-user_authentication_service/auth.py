9f9ba5433813c4b694a272b71b6e0b602db53b7c
Corbin <forstupidityonly@gmail.com>
2021-11-16 15:56:01 -0600

    fucking pycodestyle

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -11,6 +11,7 @@ def _hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
 
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
