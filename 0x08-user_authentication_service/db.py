3342b30f0b5e6156830c4d3d6e6947060bb4bb12
Corbin <forstupidityonly@gmail.com>
2021-11-16 13:28:03 -0600

    https://stackoverflow.com/questions/18110033/getting-first-row-from-sqlalchemy

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -35,3 +37,12 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """rtn the first row found in users as filtered by keyword"""
+        if not kwargs:
+            raise InvalidRequestError
+        result = self._session.query(User).filter_by(**kwargs).first()
+        if not result:
+            raise NoResultFound
+        return result
