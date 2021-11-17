b2480063b2a6dfcde5e86cff6a7876111f1fd3a8
Corbin <forstupidityonly@gmail.com>
2021-11-17 17:06:48 -0600

    im so freaking over this assignment

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -60,5 +60,18 @@ def profile():
     abort(403)
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """Resetvpassword"""
+    email = request.form.get('email')
+    if email is None:
+        abort(403)
+    try:
+        token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": token}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
