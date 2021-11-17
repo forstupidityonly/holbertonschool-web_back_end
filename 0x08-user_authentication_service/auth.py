183be7320a5ed9ff41aa05f3dd0022b1343c6508
Corbin <forstupidityonly@gmail.com>
2021-11-17 13:56:22 -0600

    task 8

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -26,3 +26,11 @@ class Auth:
             raise ValueError(f"User {email} alredy exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """credintals validator"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode('utf8'), user.hashed_password)
