9cdc58eca0b540c66fcc5ecf750013c0101c0cac
Corbin <forstupidityonly@gmail.com>
2021-11-17 16:39:57 -0600

    task 14

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -39,5 +39,16 @@ def login() -> str:
         return response
 
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """logout user"""
+    cookie_session = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(cookie_session)
+    if user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
