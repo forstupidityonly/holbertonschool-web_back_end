678396ae5ff97c5622bee368ea7aa7d4982f7672
Corbin <forstupidityonly@gmail.com>
2021-11-17 16:28:45 -0600

    task 12

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -49,3 +49,13 @@ class Auth:
             return None
         self._db.update_user(find_user.id, session_id=_generate_uuid())
         return find_user.session_id
+
+    def get_user_from_session_id(self, session_id: str) -> None:
+        """get user from id"""
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
