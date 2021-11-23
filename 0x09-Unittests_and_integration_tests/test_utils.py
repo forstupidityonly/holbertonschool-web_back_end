aac4e4989786554422d5a253f6b06fe47e357363
Corbin <forstupidityonly@gmail.com>
2021-11-23 09:54:51 -0600

    tf is patch

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,8 @@
 """test utils"""
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import Mock, patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -15,6 +16,7 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """test class"""
         self.assertEquals(access_nested_map(nested_map, path), expected)
+    
     @parameterized.expand([
         ({}, ("a",), KeyError),
         ({"a": 1}, ("a", "b"), KeyError)
@@ -22,3 +24,17 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path, expected):
         """test class"""
         self.assertRaises(expected)
+
+class TestGetJson(unittest.TestCase):
+    """test class"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('test_utils.get_json')
+    def test_get_json(self, test_url, test_payload, class_mock):
+        """test class"""
+        class_mock.return_value = test_payload
+        self.assertEquals(get_json(test_url), test_payload)
+
+
