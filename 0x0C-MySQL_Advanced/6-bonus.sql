8fcb303cb530795493951b6d7953d9183efef753
Corbin <forstupidityonly@gmail.com>
2021-12-02 16:12:01 -0600

    https://stackoverflow.com/questions/9181612/in-mysql-how-do-i-create-a-stored-procedure-that-takes-multiple-parameters

---
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,20 @@
+-- make a stored procedure that taked params
+
+DELEMITER $$
+
+
+CREATE PROCEDURE AddBonus(
+    IN user_id INT,
+    IN project_name VARCHAR(255),
+    IN score float)
+BEGIN
+    INSERT INTO projects (name)
+    SELECT project_name
+    FROM DUAL
+    WHERE project_name NOT IN (SELECT name FROM projects);
+
+    INSERT INTO Corrections (user_id, project_id, score)
+    VALUES (user_id, (SELECT id FORM projects WHERE name = project_name), score);
+END;
+
+DELIMITER;
