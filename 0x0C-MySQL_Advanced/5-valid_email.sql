c409aa46d1c325c166ef0b795eb298eba9ff1048
Corbin <forstupidityonly@gmail.com>
2021-12-02 15:45:24 -0600

    at starbuck so no testing off server unless i gotta

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+-- make trigger to reset valid_email
+
+CREATE TRIGGER trigger_name
+BEFORE UPDATE
+ON users FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
