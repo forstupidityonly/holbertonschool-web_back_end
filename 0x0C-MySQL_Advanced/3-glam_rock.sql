e3d31fc568c785ae851006a91d134a6f319ca6f8
Corbin <forstupidityonly@gmail.com>
2021-12-02 14:30:56 -0600

    all my numbers were slightly off

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
 -- find all glan rock and rank by longevity
 
-SELECT band_name, IFNULL(split, year(CURDATE())) - formed as lifespan
+SELECT band_name, IFNULL(split, 2020) - formed as lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
