e7c6b25ae6c425f1c971a10aba270310f0e014f9
Corbin <forstupidityonly@gmail.com>
2021-12-09 18:34:55 -0600

    fix

---
+++ 0x0E-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+  const array2 = [];
+  for (let idx of array) {
+    idx = appendString + idx
+    array2.push(idx);
+  }
+
+  return array2;
+}
---
+++ 0x0E-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+export default function createEmployeesObject(departmentName, employees) {
+    const employe = {
+      [departmentName]: employees,
+    };
+    return employe;
+}
