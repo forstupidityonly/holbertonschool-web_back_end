abf058b3ff2c8d1d7d1ccceebb1e495059b364b7
Corbin <forstupidityonly@gmail.com>
2021-12-16 17:30:02 -0600

    adding files

---
+++ 0x10-ES6_classes/10-car.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+export default class Car {
+	constructor(brand, motor, color) {
+		this._brand = brand;
+		this._motor = motor;
+		this._color = color;
+	}
+
+	cloneCar() {
+		return new this.constructor;
+	}
+}
---
+++ 0x10-ES6_classes/3-currency.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`
+  }
+}
