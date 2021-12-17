f32988e1577e5dedbd429661a814bd2b393f2c81
Corbin <forstupidityonly@gmail.com>
2021-12-16 23:57:39 -0600

    linter

--- 0x10-ES6_classes/7-airport.js
+++ 0x10-ES6_classes/7-airport.js
@@ -2,16 +2,16 @@
 const util = require('util');
 
 export default class Airport {
-    constructor(name, code) {
-      this._name = name;
-      this._code = code;
-    }
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+ }
 
-    [util.inspect.custom]() {
-      return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
-    }
+  [util.inspect.custom]() {
+    return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+  }
 
-    toString() {
-      return `[object ${this._code}]`;
-    }
+  toString() {
+    return `[object ${this._code}]`;
+  }
 }
