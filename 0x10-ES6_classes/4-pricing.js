fd44f27737863d809af94c8ee33f37b94f270592
Corbin <forstupidityonly@gmail.com>
2021-12-16 23:47:03 -0600

    late

--- 0x10-ES6_classes/4-pricing.js
+++ 0x10-ES6_classes/4-pricing.js
@@ -1,32 +1,32 @@
 import Currency from './3-currency';
 
 export default class Pricing {
-	constructor(amount, currency) {
-		this._amount = amount;
-		this._currency = currency;
-	}
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-	get amount() {
-		return this._amount;
-	}
+  get amount() {
+    return this._amount;
+  }
 
-	set amount(newAmount) {
-		this._amount = newAmount;
-	}
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
 
-	get currency() {
-		return this._currency;
-	}
+  get currency() {
+    return this._currency;
+  }
 
-	set currency(newCurrency) {
-		this._currency = newCurrency;
-	}
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
 
-	displayFullPrice() {
-		return `${this._amount} ${this._currency._name} (${this._currency._code})`
-	}
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`
+  }
 
-	static convertPrice(amount, conversionRate) {
-		return amount * conversionRate;
-	}
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
