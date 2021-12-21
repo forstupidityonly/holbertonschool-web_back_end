af129155cdb789e7a9c52404af7f896aeba1253f
Corbin <forstupidityonly@gmail.com>
2021-12-21 15:34:48 -0600

    bananas spelled wrong and linter issues

--- 0x11-ES6_data_manipulation/9-groceries_list.js
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -1,2 +1,2 @@
-const groceriesList = () => new Map([['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banna', 5]]);
-export default groceriesList
+const groceriesList = () => new Map([['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]]);
+export default groceriesList;
