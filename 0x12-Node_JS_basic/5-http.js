ce5e4dde0fba566f160437a484edd0add5c7286b
Corbin <forstupidityonly@gmail.com>
2022-01-03 18:23:41 -0600

    i hope the checker helps me troubleshoot task 5

---
+++ 0x12-Node_JS_basic/5-http.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+const port = 1245;
+
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      res.end('Hello Holberton School!');
+    } else if (req.url === '/students') {
+      await countStudents(process.argv[2])
+        .then((val) => {
+          res.write('This is the list of our students\n');
+          res.write(`Number of students: ${val.lines.length}\n`);
+          res.write(`Number of students in CS: ${val.findCS.length}. List: ${val.findCS.join(', ')}\n`);
+          res.write(`Number of students in SWE: ${val.findSWE.length}. List: ${val.findSWE.join(', ')}\n`);
+          res.end();
+        })
+        .catch((err) => {
+          res.write('This is the list of our students\n');
+          res.end(err.message);
+        });
+    }
+  }
+});
+app.listen(port);
+module.exports = app;
