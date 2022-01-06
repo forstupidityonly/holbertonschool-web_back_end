01961aabd3d87e103b2e0164c9c4c414f18c46c2
Corbin <forstupidityonly@gmail.com>
2022-01-05 20:05:56 -0600

    more freaking tests???

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require("assert");
-const calculateNumber = require("./0-calcul.js");
+const calculateNumber = require("./0-calcul");
 
 describe("#calculateNumber()", function () {
   it("should return 4 when calculateNumber is passed 1 and 3", function () {
@@ -13,5 +13,14 @@ describe("#calculateNumber()", function () {
   });
   it("should return 6 when calculateNumber is passed 1.5 and 3.7", function () {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+  it("should return 0 when calculateNumber is passed -0.8 and 0.8", function () {
+    assert.strictEqual(calculateNumber(-0.8, 0.8), 0);
+  });
+  it("should return 0 when calculateNumber is passed 0.1 and 0.2", function () {
+    assert.strictEqual(calculateNumber(0.1, 0.2), 0);
+  });
+  it("should return -3 when calculateNumber is passed -1.6 and -0.7", function () {
+    assert.strictEqual(calculateNumber(-1.6, -0.7), 3);
   });
 });
